47e21d34192030d8ea74892449a46ebd0e2e191c
K <k@Ks-MacBook-Pro.local>
2020-04-08 13:14:44 +0200

    add node builder

--- src/Generator.php
+++ src/Generator.php
@@ -4,6 +4,9 @@
 namespace App;
 
 
+use _HumbugBox09702017065e\Roave\BetterReflection\Identifier\Identifier;
+use PhpParser\Builder\Method;
+use PhpParser\BuilderFactory;
 use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Stmt\Expression;
@@ -44,21 +47,38 @@ class Generator
 
 
                 $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+
+
                 ini_set('xdebug.max_nesting_level', 3000);
                 $content = $parser->parse($file->getContents());
 
                 $traverser = new NodeTraverser();
 
+
+
+
                 $traverser->addVisitor(new class extends NodeVisitorAbstract {
+
+
                     public function enterNode(Node $node)
                     {
+                        $factory = new BuilderFactory;
+
                         if ($node instanceof Node\Stmt\Class_) {
+
+
                             $node->name = new Node\Identifier($node->name . 'Test');
-                            $node->extends = New Node\Identifier('\PHPUnit\TestCase');
+                            $node->extends = New Node\Identifier('TestCase');
                             $node->flags = 1;
                         }
 
+
                         if ($node instanceof Node\Stmt\ClassMethod) {
+
+                            $testMethod = new Node\Stmt\ClassMethod( New Node\Identifier('Test'));
+                            $testMethod->flags = 1;
+
+                            $node->stmts[] = $testMethod;
 
                         }
 
